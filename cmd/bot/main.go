91c5f0d2e44b3717d6403cac6dabcb2e2386298e
jew <you@example.com>
2024-10-31 18:06:38 +0300

    add tgbot sdc

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,6 +1,39 @@
 package main
 
-func main() {
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
 
-	println("asasd")
+func main() {
+	godotenv.Load()
+	tgToken := os.Getenv("TOKEN")
+
+	bot, err := tgbotapi.NewBotAPI(tgToken)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.UpdateConfig{
+		Timeout: 60,
+	}
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil { // If we got a message
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "You wrote: "+update.Message.Text)
+
+			bot.Send(msg)
+		}
+	}
 }
